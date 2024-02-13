361a1f30467b355bd08dc77f7fa565e50ba15e09
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2024-02-13 11:36:35 +0600

    add js1

---
+++ js1/dz.js
@@ -0,0 +1,43 @@
+// 1
+console.log('\t\t\t Задание №1');
+const num = prompt('Please enter a number')
+// or
+// const num = 10;
+console.log(num);
+
+// 2
+let a = 10, b = 2, c = 5;
+console.log("\n\t\t\t Задание №2", '\nСумма = ', a + b + c);
+
+// 3
+const per = 100;
+console.log("\n\t\t\t Задание №3", '\n', per + ' * 2 = ' + (per * 2));
+
+// 4
+let per4 = 67;
+console.log("\n\t\t\t Задание №4", '\n', per4 + ' % 7 = ' + (per4 % 7));
+
+// 5
+let a5 = 10, b5 = 2;
+console.log("\n\t\t\t Задание №5", '\n' + a5 + ' + ' + b5 + ' = ' + (a5 + b5));
+console.log(a5 + ' - ' + b5 + ' = ' + (a5 - b5));
+console.log(a5 + ' * ' + b5 + ' = ' + (a5 * b5));
+console.log(a5 + ' / ' + b5 + ' = ' + (a5 / b5));
+
+// 6
+const c6 = 15, d6 = 2;
+const result = c6 + d6;
+console.log("\n\t\t\t Задание №6", '\nresult = ', result);
+
+// 7
+let a7 = 30, b7 = 15;
+const c7 = a7 + b7;
+console.log(`\n\t\t\t Задание №7\n${c7} % 4 = ${c7 % 4}`);
+
+// 8
+let a8 = 17, b8 = 10;
+const c8 = a8 - b8;
+let d8 = 17;
+const result8 = c8 + d8;
+console.log(`\n\t\t\t Задание №8\nresult = ${result8}`);
+
---
+++ js1/main.js
@@ -0,0 +1,52 @@
+// console.log("hello world!");
+// console.log("this is second console");
+// alert("hello world!");
+// console.log(4
+//     + 4
+//     + 2)
+
+// alert("hello world!");
+// [1, 2].forEach(alert);
+
+// console.log("check comment"); // console
+// console.log("check comment");
+
+// // переменный
+
+// // let
+
+// let message;
+// message = 'this is message text';
+// console.log(message);
+
+// let student = "Diana";
+// console.log(student);
+
+// let mentor = "ayanan";
+// console.log(mentor);
+
+// mentor = "Aizat";
+// console.log(mentor);
+// let mentorAge = 30;
+// console.log(mentorAge);
+
+// const
+
+// const carPrice = 10000;
+
+// console.log('carPrice = ', carPrice);
+
+// var
+// var month 
+// month = 'february'
+// console.log('month = ', month);
+// month = 'march'
+// console.log(month);
+
+let n = prompt('enter name')
+const surName = prompt('enter surName')
+
+var question = confirm('Вам 18 лет?')
+console.log(question);
+
+alert("hello mr " + n + ' ' + surName + ' ' + question);
