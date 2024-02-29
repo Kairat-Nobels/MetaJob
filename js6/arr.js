c9962c4bf08f527f4c6965e638c6ab2ec66052b8
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2024-02-29 12:42:07 +0600

    add home6

---
+++ js6/arr.js
@@ -0,0 +1,16 @@
+// ? arrays
+// [1,2,3,4,5, "down", false, {title: "me"}]
+// let arr = [];
+// console.log(arr);
+
+// let arr2 = new Array()
+// console.log(arr2);
+
+let arr = ["a", "b", "c", "d"]
+console.log(arr);
+let rez = arr.indexOf("c")
+console.log(rez);
+
+let array = [1, 3, 45, 67, "dad", false, undefined, null, NaN];
+console.log(array.at(-1));
+console.log(array.find(el => el === 0));
---
+++ js6/main.js
@@ -0,0 +1,107 @@
+// Логические операторы
+
+// || - или
+// && - и
+// ! - не
+// ?? - оператор нулевого слияния
+
+// result = a || b
+// 1 - true
+// 0 - false
+// console.log("true || true: ", true || true)
+// console.log("true || false: ", true || false)
+// console.log("false || true: ", false || true)
+// console.log("false || false: ", false || false)
+// console.log("false || true || false: ", false || true || false)
+
+// let bread = false;
+// let milk = false;
+
+// console.log(bread || milk);
+
+// let first = 0, second = 0, third = undefined;
+// let result = first || second || third;
+// alert(result)
+
+// if ("" || 0 || undefined) console.log("hello");
+// else console.log('object');
+
+// let user1 = {
+//     name: "Tariel",
+//     mentor: true
+// }
+
+// let user2 = {
+//     name: "Bektur",
+//     mentor: false
+// }
+
+// console.log(user2.mentor ? "You are mentor" : "You are not")
+
+// let hour = 9;
+// if (hour < 10 || hour > 18) console.log("офис закрыт");
+
+//? && (и)
+// result = a && b
+
+// console.log("true && true: ", true && true)
+// console.log("true && false: ", true && false)
+// console.log("false && true: ", false && true)
+// console.log("false && false: ", false && false);
+// console.log("false && true && false: ", false && true && false)
+
+// let user1 = {
+//     name: "Tariel",
+//     mentor: true
+// }
+
+// let user2 = {
+//     name: "Bektur",
+//     mentor: true
+// }
+
+// console.log((user2.mentor && user1.mentor) ? "You are mentor" : "You are not")
+
+// let third = 'text';
+// let second = true;
+// let first = 1;
+// let four = 23;
+
+// let result = third && first || second;
+// alert(result)
+
+// !! - (не)
+
+// alert(!!"kaira")
+// alert(!!null)
+
+// ------------------------
+// let value1 = "kaira"
+// let value2 = NaN;
+// let value3 = undefined;
+
+// if (value1 && value2 || !value3) {
+//     console.log("Верное значение");
+// } else {
+//     console.log("неверное значение");
+// }
+
+// console.log(true || false && true || true);
+
+
+// ? Оператор нулевого присваивания (??)
+// const result = undefined ?? "Kairat"
+// console.log(result);
+
+// let count;
+// count = 1;
+// count = NaN;
+// count = null;
+
+// let checkVariableCount = count ?? 100;
+// console.log(checkVariableCount);
+
+// console.log(null ?? "+");
+// console.log("-" ?? "+");
+
+
